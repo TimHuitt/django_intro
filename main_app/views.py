2e035f4688e1b7449ec048bbdb5a6dcad088349c
Tim Huitt <timhuitt@gmail.com>
2024-01-10 08:29:35 -0700

    complete lesson 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from .models import Cat
 
-# cats = [
-#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-# ]
-
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
@@ -17,4 +12,10 @@ def cats_index(request):
   cats = Cat.objects.all()
   return render(request, 'cats/index.html', {
     'cats': cats
+  })
+
+def cat_details(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/details.html', {
+    'cat': cat
   })
